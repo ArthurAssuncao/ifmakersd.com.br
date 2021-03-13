2e6512d50fe4215dd0d8bf3c9521db8cf9ad38d0
Arthur Assuncao <arthur.xx92@gmail.com>
2021-03-13 17:50:18 -0300

    feat: add DarThemeButton

---
+++ src/components/DarkThemeButton/DarkModeButton.tsx
@@ -0,0 +1,42 @@
+import moonLine from "@iconify/icons-clarity/moon-line";
+import sunLine from "@iconify/icons-clarity/sun-line";
+import { Icon } from "@iconify/react";
+import { useEffect, useState } from "react";
+import styles from "./DarkModeButton.module.scss";
+
+const DarkModeButton = () => {
+  const [theme, setTheme] = useState("light");
+  const [isAnimationActive, setAnimationActive] = useState(false);
+
+  useEffect(() => {
+    document.body.dataset.theme = theme;
+  }, [theme]);
+
+  const changeTheme = () => {
+    setAnimationActive(true);
+    setTheme(theme === "light" ? "dark" : "light");
+
+    setTimeout(() => {
+      setAnimationActive(false);
+    }, 600);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={`${styles.containerInner} ${
+          isAnimationActive ? styles.animation : ""
+        }`}
+      >
+        <div className={styles.iconWrapper} onClick={() => changeTheme()}>
+          <Icon icon={moonLine} className={styles.icon} />
+        </div>
+        <div className={styles.iconWrapper} onClick={() => changeTheme()}>
+          <Icon icon={sunLine} className={styles.icon} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { DarkModeButton };
